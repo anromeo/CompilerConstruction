4986d2a2fd3ee3ba863045e257e7446201029c6e
quinncox <qlcforever@hotmail.com>
2016-04-29 10:36:34 -0700

    added throw and throws

---
+++ tests/pass/Throw.java
@@ -0,0 +1,8 @@
+
+public class Throw {
+	
+	public void testThrow() {
+		throw new throwableExpression();
+	}
+
+}
---
+++ tests/pass/Throws.java
@@ -0,0 +1,6 @@
+
+public class Throws {
+	void testThrows() throws aThrowsException {
+		
+	}
+}
